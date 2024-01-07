6199d1172e606014c82fcd02530e34a58a91de40
Gustavo Alves Miyazaki <gustavo.alves388@gmail.com>
2024-01-07 18:17:00 -0300

    Add Lazygit and gitblame

--- nvim/lua/custom/settings/init.lua
+++ nvim/lua/custom/settings/init.lua
@@ -1,4 +1,4 @@
-local utils = require('custom.utils')
+local utils = require 'custom.utils'
 
 -- Defaults
 vim.opt.tabstop = 2
@@ -11,24 +11,25 @@ vim.opt.scrolloff = 4
 
 --theme
 vim.cmd.colorscheme 'tokyonight'
--- vim.cmd.highlight 'Normal guibg=none'
 vim.api.nvim_set_hl(0, 'iCursor', { foreground = '#ffffff', background = '#CD0058' })
 vim.opt.guicursor = 'i:block-iCursor'
 vim.api.nvim_set_hl(0, 'CursorLine', { background = '#233745' })
-vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
-vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
-vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
--- vim.api.nvim_set_hl(0, "BufferLineFill", { background = "#24283B", foreground = "#565f89" })
+vim.api.nvim_set_hl(0, 'IlluminatedWordText', { link = 'Visual' })
+vim.api.nvim_set_hl(0, 'IlluminatedWordRead', { link = 'Visual' })
+vim.api.nvim_set_hl(0, 'IlluminatedWordWrite', { link = 'Visual' })
+-- git-blame
+vim.g.gitblame_highlight_group = 'GitBlame'
+vim.api.nvim_set_hl(0, 'GitBlame', { background = '#233745', foreground = '#565f89' })
 
 --file explorer
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
 
 -- copilot
 vim.g.copilot_no_tab_map = true
-vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.api.nvim_set_keymap('i', '<C-J>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
 
 --auto-resize
-vim.api.nvim_command('autocmd VimResized * wincmd =')
+vim.api.nvim_command 'autocmd VimResized * wincmd ='
 
 vim.api.nvim_set_keymap('n', '<C-w>s', '<C-w>v', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-w>v', '<C-w>s', { noremap = true, silent = true })
@@ -37,13 +38,13 @@ vim.api.nvim_set_keymap('n', '<C-w>v', '<C-w>s', { noremap = true, silent = true
 vim.api.nvim_set_keymap('n', '<leader>zz', ':let &scrolloff=999-&scrolloff<CR>', { noremap = true, silent = true })
 
 --disable auto comment on newline
-vim.cmd('autocmd BufEnter * set formatoptions-=o')
+vim.cmd 'autocmd BufEnter * set formatoptions-=o'
 
 --enable cursorline
 vim.opt.cursorline = true
 
 --remove alacritty padding
-local alacritty_config_file = "/home/gmiyazaki/.config/alacritty/alacritty.toml";
+local alacritty_config_file = '/home/gmiyazaki/.config/alacritty/alacritty.toml'
 
 local function remove_alacritty_padding()
   utils.Sad('07', 16, 0, alacritty_config_file)
@@ -53,28 +54,28 @@ end
 local function reset_alacritty_padding()
   -- verify if there is no other nvim instance running
   -- each nvim instance will add 2 to the count
-  local nvim_count = vim.fn.system("pgrep -c nvim")
+  local nvim_count = vim.fn.system 'pgrep -c nvim'
   if tonumber(nvim_count) >= 3 then
-    vim.cmd('sleep 100m')
+    vim.cmd 'sleep 100m'
     return
   end
   utils.Sad('07', 0, 16, alacritty_config_file)
   utils.Sad('08', 0, 16, alacritty_config_file)
-  vim.cmd('sleep 100m')
+  vim.cmd 'sleep 100m'
 end
 
-local alacritty_au = vim.api.nvim_create_augroup("alacritty_padding_au", {
-  clear = true
+local alacritty_au = vim.api.nvim_create_augroup('alacritty_padding_au', {
+  clear = true,
 })
 
-vim.api.nvim_create_autocmd({ "VimEnter" }, {
-  pattern = { "*" },
+vim.api.nvim_create_autocmd({ 'VimEnter' }, {
+  pattern = { '*' },
   callback = remove_alacritty_padding,
-  group = alacritty_au
+  group = alacritty_au,
 })
 
-vim.api.nvim_create_autocmd({ "VimLeave" }, {
-  pattern = { "*" },
+vim.api.nvim_create_autocmd({ 'VimLeave' }, {
+  pattern = { '*' },
   callback = reset_alacritty_padding,
-  group = alacritty_au
+  group = alacritty_au,
 })
