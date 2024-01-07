f7a962f71608f1ac383b68671c8bc16a1a198538
Gustavo Alves Miyazaki <gustavo.alves388@gmail.com>
2024-01-07 18:27:26 -0300

    add automation folder and verify if is alacritty before trying to remove padding

--- nvim/init.lua
+++ nvim/init.lua
@@ -476,6 +476,7 @@ cmp.setup {
 }
 
 require 'custom.settings'
+require 'custom.automation.init'
 --
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- nvim/lua/custom/settings/init.lua
+++ nvim/lua/custom/settings/init.lua
@@ -1,5 +1,3 @@
-local utils = require 'custom.utils'
-
 -- Defaults
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
@@ -42,40 +40,3 @@ vim.cmd 'autocmd BufEnter * set formatoptions-=o'
 
 --enable cursorline
 vim.opt.cursorline = true
-
---remove alacritty padding
-local alacritty_config_file = '/home/gmiyazaki/.config/alacritty/alacritty.toml'
-
-local function remove_alacritty_padding()
-  utils.Sad('07', 16, 0, alacritty_config_file)
-  utils.Sad('08', 16, 0, alacritty_config_file)
-end
-
-local function reset_alacritty_padding()
-  -- verify if there is no other nvim instance running
-  -- each nvim instance will add 2 to the count
-  local nvim_count = vim.fn.system 'pgrep -c nvim'
-  if tonumber(nvim_count) >= 3 then
-    vim.cmd 'sleep 100m'
-    return
-  end
-  utils.Sad('07', 0, 16, alacritty_config_file)
-  utils.Sad('08', 0, 16, alacritty_config_file)
-  vim.cmd 'sleep 100m'
-end
-
-local alacritty_au = vim.api.nvim_create_augroup('alacritty_padding_au', {
-  clear = true,
-})
-
-vim.api.nvim_create_autocmd({ 'VimEnter' }, {
-  pattern = { '*' },
-  callback = remove_alacritty_padding,
-  group = alacritty_au,
-})
-
-vim.api.nvim_create_autocmd({ 'VimLeave' }, {
-  pattern = { '*' },
-  callback = reset_alacritty_padding,
-  group = alacritty_au,
-})
