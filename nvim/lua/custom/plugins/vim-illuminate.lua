a50349e7627bb7dd96508b0a4a9c486815582eae
Gustavo Alves Miyazaki <gustavo.alves388@gmail.com>
2024-01-27 19:13:03 -0300

    fix: illuminate goto next and prev symbol

--- nvim/lua/custom/plugins/vim-illuminate.lua
+++ nvim/lua/custom/plugins/vim-illuminate.lua
@@ -1,13 +1,13 @@
 return {
   'RRethy/vim-illuminate',
   config = function()
-    local illuminate = require('illuminate')
+    local illuminate = require 'illuminate'
 
     vim.keymap.set('n', '[[', function()
       illuminate.goto_prev_reference(false)
-    end, { noremap = true, silent = true })
+    end, { noremap = true, silent = true, desc = 'Previous symbol match' })
     vim.keymap.set('n', ']]', function()
       illuminate.goto_next_reference(false)
-    end, { noremap = true, silent = true })
+    end, { noremap = true, silent = true, desc = 'Next symbol match' })
   end,
 }
