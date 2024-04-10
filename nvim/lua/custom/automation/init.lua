ec5eaff72b178c64c8431e42116df4157cecd985
Gustavo Alves Miyazaki <gustavo.alves388@gmail.com>
2024-04-09 22:58:17 -0300

    refactor: change alacritty padding removal automation to make it remove the padding only on nvim terminal sessions instead of all

--- nvim/lua/custom/automation/init.lua
+++ nvim/lua/custom/automation/init.lua
@@ -35,16 +35,18 @@ local _alacritty_au = vim.api.nvim_create_augroup('alacritty_padding_au', {
   clear = true,
 })
 
-vim.api.nvim_create_autocmd({ 'VimEnter' }, {
-  pattern = { '*' },
-  callback = _remove_alacritty_padding,
+vim.api.nvim_create_autocmd('VimEnter', {
   group = _alacritty_au,
+  callback = function()
+    vim.fn.system "alacritty msg --socket $ALACRITTY_SOCKET config -w $ALACRITTY_WINDOW_ID options 'window.padding.x=0' 'window.padding.y=0' 'window.dynamic_padding=false'"
+  end,
 })
 
-vim.api.nvim_create_autocmd({ 'VimLeave' }, {
-  pattern = { '*' },
-  callback = _reset_alacritty_padding,
+vim.api.nvim_create_autocmd('VimLeavePre', {
   group = _alacritty_au,
+  callback = function()
+    vim.fn.jobstart('alacritty msg --socket $ALACRITTY_SOCKET config -w $ALACRITTY_WINDOW_ID -r', { detach = true })
+  end,
 })
 
 --Set filetype to groovy for jenkinsfiles
