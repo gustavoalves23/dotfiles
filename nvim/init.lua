b8eefb48b706ef2bd77be5fe820a2ddc419029cd
Gustavo Alves Miyazaki <gustavo.alves388@gmail.com>
2024-04-12 18:11:36 -0300

    feat: move lazy config to a separated file

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,22 +1,7 @@
 vim.g.mapleader = ' '
 
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
-    lazypath,
-  }
-end
-
-vim.opt.rtp:prepend(lazypath)
-
-require('lazy').setup({ { import = 'plugins' } }, {})
+require 'lazy'
 require 'settings'
-
 require 'langs.syntax_highlight'
 require 'langs.language_server'
 require 'langs.completion'
