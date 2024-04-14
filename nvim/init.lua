5be39ccd0a0df5d4af939194975b89a2f3218116
Gustavo Alves Miyazaki <gustavo.alves388@gmail.com>
2024-04-14 18:55:42 -0300

    feat: Add comments on init file

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,14 @@
 vim.g.mapleader = ' '
 
+-- Initialize lazy.nvim and load the plugins
 require 'bootstrap'
+-- Define native nvim and plugins settings and keymaps
 require 'settings'
+-- Configure language specific definitions and capabilities
 require 'langs.syntax_highlight'
 require 'langs.language_server'
 require 'langs.completion'
 require 'langs.lint'
 require 'langs.debug'
-
+-- Configure custom automations
 require 'automation'
