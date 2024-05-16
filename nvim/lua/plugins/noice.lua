f128e0bcd33b7c1acc666d0366d244702c4e5371
Gustavo Alves Miyazaki <gustavo.alves388@gmail.com>
2024-05-16 13:08:48 -0300

    feat: return nvim-notify

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,53 +1,21 @@
 return {
   'folke/noice.nvim',
-  -- event = 'VeryLazy',
+  event = 'VeryLazy',
   dependencies = {
     'MunifTanjim/nui.nvim',
+    {
+      "rcarriga/nvim-notify",
+      event = "VeryLazy",
+      opts = {
+        stages = "static",
+        timeout = 3000,
+        on_open = function(win)
+          vim.api.nvim_win_set_config(win, { zindex = 100 })
+        end
+      },
+    }
   },
   opts = {
-    views = {
-      mini = {
-        position = {
-          row = 1,
-          col = '100%',
-        },
-        lsp_progress = {},
-      },
-    },
-    lsp = {
-      progress = {
-        enabled = true,
-        format = 'lsp_progress',
-        format_done = {
-          { '󰄬 ', hl_group = 'NoiceLspProgressSpinner' },
-          { '{data.progress.title} ', hl_group = 'NoiceLspProgressTitle' },
-          { '{data.progress.client} ', hl_group = 'NoiceLspProgressClient' },
-        },
-        throttle = 1000 / 30,
-        view = 'mini',
-      },
-      hover = {
-        silent = true,
-      },
-      override = {
-        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-        ['vim.lsp.util.stylize_markdown'] = true,
-        ['cmp.entry.get_documentation'] = true,
-      },
-    },
-    routes = {
-      {
-        filter = {
-          event = 'msg_show',
-          any = {
-            { find = '%d+L, %d+B' },
-            { find = '; after #%d+' },
-            { find = '; before #%d+' },
-          },
-        },
-        view = 'mini',
-      },
-    },
     presets = {
       bottom_search = true,
       command_palette = true,
@@ -57,26 +25,11 @@ return {
   },
   keys = {
     {
-      '<S-Enter>',
-      function()
-        require('noice').redirect(vim.fn.getcmdline())
-      end,
-      mode = 'c',
-      desc = 'Redirect Cmdline',
-    },
-    {
       '<leader>snl',
       function()
         require('noice').cmd 'last'
       end,
       desc = 'Noice Last Message',
-    },
-    {
-      '<leader>sna',
-      function()
-        require('noice').cmd 'all'
-      end,
-      desc = 'Noice All',
     },
     {
       '<leader>snd',
@@ -85,29 +38,5 @@ return {
       end,
       desc = 'Dismiss All',
     },
-    {
-      '<c-f>',
-      function()
-        if not require('noice.lsp').scroll(4) then
-          return '<c-f>'
-        end
-      end,
-      silent = true,
-      expr = true,
-      desc = 'Scroll forward',
-      mode = { 'i', 'n', 's' },
-    },
-    {
-      '<c-b>',
-      function()
-        if not require('noice.lsp').scroll(-4) then
-          return '<c-b>'
-        end
-      end,
-      silent = true,
-      expr = true,
-      desc = 'Scroll backward',
-      mode = { 'i', 'n', 's' },
-    },
-  },
+  }
 }
