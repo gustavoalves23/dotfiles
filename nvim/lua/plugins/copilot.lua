b7f4bc57960d0648e08e54538b55669dba15ce2c
Gustavo Alves Miyazaki <gustavo.alves388@gmail.com>
2024-05-17 23:31:27 -0300

    feat: Add command to trigger manually copilot suggestion

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -2,7 +2,7 @@
 return {
   "zbirenbaum/copilot.lua",
   opts = {
-    suggestion = { enabled = true, auto_trigger = true, keymap = { accept = "<C-j>" } },
+    suggestion = { enabled = true, auto_trigger = true },
     panel = {
       enabled = true,
       auto_refresh = false,
@@ -22,4 +22,18 @@ return {
       help = true,
     },
   },
+  config = function(_, opts)
+    local copilot = require("copilot")
+    copilot.setup(opts)
+
+    local suggestion = require("copilot.suggestion")
+
+    vim.keymap.set('i', '<C-j>', function()
+      if suggestion.is_visible() then
+        suggestion.accept()
+      else
+        suggestion.next()
+      end
+    end)
+  end
 }
