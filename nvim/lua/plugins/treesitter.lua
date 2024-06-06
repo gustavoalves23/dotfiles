76eb0ff8e5f9220ccff0e4f348af0506e4cc9fb9
Gustavo Alves Miyazaki <gustavo.alves388@gmail.com>
2024-06-05 22:31:05 -0300

    fix: Remove unused config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,22 +1,7 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  -- commit = "fa1515b015ab9894aa9866410b1c23e03d2be4af",
   dependencies = {
     'nvim-treesitter/nvim-treesitter-textobjects',
   },
   build = ':TSUpdate',
-  opts = {
-    context_commentstring = {
-      config = {
-        javascript = {
-          __default = '// %s',
-          jsx_element = '{/* %s */}',
-          jsx_fragment = '{/* %s */}',
-          jsx_attribute = '// %s',
-          comment = '// %s',
-        },
-        typescript = { __default = '// %s', __multiline = '/* %s */' },
-      },
-    },
-  },
 }
