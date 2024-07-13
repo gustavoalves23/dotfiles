9d88066813f102e8da532cea55ae75e22168a2cd
Gustavo Alves Miyazaki <gustavo.alves388@gmail.com>
2024-07-12 21:10:53 -0300

    feat: return copilot

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,27 +1,10 @@
--- return { 'github/copilot.vim' }
 return {
   'zbirenbaum/copilot.lua',
-  enabled = false,
   opts = {
     suggestion = { enabled = true, auto_trigger = true },
     panel = {
-      enabled = true,
-      auto_refresh = false,
-      keymap = {
-        jump_prev = '[[',
-        jump_next = ']]',
-        accept = '<CR>',
-        refresh = 'gr',
-      },
-      layout = {
-        position = 'bottom',
-        ratio = 0.4,
-      },
-    },
-    filetypes = {
-      markdown = true,
-      help = true,
-    },
+      enabled = false,
+    }
   },
   config = function(_, opts)
     local copilot = require 'copilot'
