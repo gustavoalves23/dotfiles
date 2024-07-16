95f96349a2226c506f24d81c74e7525632d41a98
Gustavo Alves Miyazaki <gustavo.alves388@gmail.com>
2024-07-15 22:11:55 -0300

    feat: remove unused keymap

--- nvim/lua/langs/completion.lua
+++ nvim/lua/langs/completion.lua
@@ -73,16 +73,7 @@ cmp.setup {
       else
         fallback()
       end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.locally_jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
+    end, { 'i', 's' })
   },
   sources = {
     { name = 'nvim_lsp' },
