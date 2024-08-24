23df331357113089b1be202b87bce6fff19bb944
Gustavo Alves Miyazaki <gustavo.alves388@gmail.com>
2024-08-24 20:12:09 -0300

    feat: Try to fix svelte lsp update on changes on ts/js files

--- nvim/lua/langs/configs/svelte.lua
+++ nvim/lua/langs/configs/svelte.lua
@@ -1,6 +1,8 @@
 return {
   svelte = {
-    filetypes = { 'svelte' },
+    filetypes = {
+      'svelte',
+    },
     snippets = {
       extends = {
         'typescript',
@@ -11,12 +13,24 @@ return {
         svelte = {
           on_attach = function(client)
             vim.api.nvim_create_autocmd('BufWritePost', {
-              pattern = { '*.js', '*.ts' },
+              pattern = {
+                '*.js',
+                '*.ts',
+              },
               callback = function(ctx)
-                if client.name == 'svelte' then
-                  client.notify('$/onDidChangeTsOrJsFile', { uri = ctx.file })
-                end
+                client.notify('$/onDidChangeTsOrJsFile', {
+                  uri = ctx.match,
+                })
+                vim.notify(client.name .. ' server reloading for file ' .. ctx.match, vim.log.levels.INFO)
               end,
+            })
+            vim.api.nvim_create_user_command('SvelteReload', function()
+              client.notify('$/onDidChangeSvelteFile', {
+                uri = vim.api.nvim_buf_get_name(0),
+              })
+              vim.notify(client.name .. ' server reloading for file ' .. vim.api.nvim_buf_get_name(0), vim.log.levels.INFO)
+            end, {
+              nargs = 0,
             })
           end,
         },
