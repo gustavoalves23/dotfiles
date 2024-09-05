0da59069b16f00626bff55956b8ad9cc0cd53c28
Gustavo Alves Miyazaki <gmiyazaki@uolinc.com>
2024-09-05 19:49:36 -0300

    feat: refactor alacritty padding automation

--- nvim/lua/automation/init.lua
+++ nvim/lua/automation/init.lua
@@ -1,44 +1,38 @@
 local _is_alacritty = vim.env.ALACRITTY_WINDOW_ID ~= nil
 
-local _alacritty_au = vim.api.nvim_create_augroup('alacritty_padding_au', {
-  clear = true,
-})
+if _is_alacritty then
+  local _alacritty_au = vim.api.nvim_create_augroup('alacritty_padding_au', {
+    clear = true,
+  })
 
-vim.api.nvim_create_autocmd('VimEnter', {
-  group = _alacritty_au,
-  callback = function()
-    if not _is_alacritty then
-      return
-    end
+  local function add_padding()
     vim.fn.system "alacritty msg --socket $ALACRITTY_SOCKET config -w $ALACRITTY_WINDOW_ID options 'window.padding.x=0' 'window.padding.y=0' 'window.dynamic_padding=false'"
+  end
 
-    vim.api.nvim_create_user_command("AlacrittyPadding", function()
-    vim.fn.system "alacritty msg --socket $ALACRITTY_SOCKET config -w $ALACRITTY_WINDOW_ID options 'window.padding.x=0' 'window.padding.y=0' 'window.dynamic_padding=false'"
-    end, {})
-  end,
-  once = true,
-})
+  vim.api.nvim_create_user_command('AlacrittyPadding', add_padding, {})
+  vim.api.nvim_create_autocmd('VimEnter', {
+    group = _alacritty_au,
+    callback = add_padding,
+    once = true,
+  })
 
-vim.api.nvim_create_autocmd('VimLeavePre', {
-  group = _alacritty_au,
-  callback = function()
-    local function run()
-      if not _is_alacritty then
-        return
-      end
-      if vim.env.TMUX then
-        local nvim_session_count = vim.fn.system(vim.fn.stdpath 'config' .. '/lua/automation/scripts/tmux_nvim_sessions.sh')
-        if tonumber(nvim_session_count) > 1 then
-          return
+  vim.api.nvim_create_autocmd('VimLeavePre', {
+    group = _alacritty_au,
+    callback = function()
+      local function run()
+        if vim.env.TMUX then
+          local nvim_session_count = vim.fn.system(vim.fn.stdpath 'config' .. '/lua/automation/scripts/tmux_nvim_sessions.sh')
+          if tonumber(nvim_session_count) > 1 then
+            return
+          end
         end
+        vim.fn.jobstart('alacritty msg --socket $ALACRITTY_SOCKET config -w $ALACRITTY_WINDOW_ID -r', { detach = true })
       end
-      vim.fn.jobstart('alacritty msg --socket $ALACRITTY_SOCKET config -w $ALACRITTY_WINDOW_ID -r', { detach = true })
-      vim.cmd 'sleep 100m'
-    end
-    pcall(run)
-  end,
-  once = true,
-})
+      pcall(run)
+    end,
+    once = true,
+  })
+end
 
 --Set filetype to groovy for jenkinsfiles
 vim.filetype.add {
