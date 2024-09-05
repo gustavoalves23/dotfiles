2b90fc582bc1320daba3ae0353e284be3a8a3efc
Gustavo Alves Miyazaki <gmiyazaki@uolinc.com>
2024-09-05 19:28:11 -0300

    feat: Add sway config

--- rofi/powermenu.sh
+++ rofi/powermenu.sh
@@ -76,6 +76,8 @@ run_cmd() {
 				openbox --exit
 			elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
 				bspc quit
+			elif [[ "$DESKTOP_SESSION" == 'sway-with-vars' ]]; then
+				swaymsg exit
 			elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
 				i3-msg exit
 			elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
---
+++ sway/scripts/display-principal.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Get all active outputs
+outputs=$(swaymsg -t get_outputs | jq -r '.[] | select(.active) | .name')
+
+# Check if eDP-1 and DP-1 are in the list of active outputs
+edp_active=$(echo "$outputs" | grep -w "eDP-1")
+dp_active=$(echo "$outputs" | grep -w "DP-1")
+
+if [[ -n "$edp_active" && -n "$dp_active" ]]; then
+    # Both eDP-1 and DP-1 are active, disable eDP-1
+    swaymsg output eDP-1 disable
+    swaymsg output DP-1 enable
+elif [[ -n "$edp_active" && -z "$dp_active" ]]; then
+    # Only eDP-1 is active, disable eDP-1 and enable DP-1
+    swaymsg output eDP-1 disable
+    swaymsg output DP-1 enable
+elif [[ -z "$edp_active" && -n "$dp_active" ]]; then
+    # Only DP-1 is active, disable DP-1 and enable eDP-1
+    swaymsg output DP-1 disable
+    swaymsg output eDP-1 enable
+else
+    notify-send "No known outputs are active."
+fi
+
+pkill gammastep && sleep 1 && gammastep -O 3000k;
