be5cef84a26322ec12a55a1a64154f65da6da979
Gustavo Alves Miyazaki <gustavo.alves388@gmail.com>
2024-11-12 23:03:08 -0300

    feat: return supermaven

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,27 +1,44 @@
 return {
-  'zbirenbaum/copilot.lua',
-  event = 'VeryLazy',
+  'supermaven-inc/supermaven-nvim',
   opts = {
-    suggestion = { enabled = true, auto_trigger = true },
-    panel = {
-      enabled = false,
+    ignore_filetypes = {
+      'oil',
+      'TelescopePrompt',
+      'TelescopeResults',
+      'rust',
     },
-    filetypes = {
-      rust = false,
+    keymaps = {
+      accept_suggestion = '<C-j>',
+      clear_suggestion = '<C-]>',
+      accept_word = '<C-z>',
     },
   },
-  config = function(_, opts)
-    local copilot = require 'copilot'
-    copilot.setup(opts)
-
-    local suggestion = require 'copilot.suggestion'
-
-    vim.keymap.set('i', '<C-j>', function()
-      if suggestion.is_visible() then
-        suggestion.accept()
-      else
-        suggestion.next()
-      end
-    end)
-  end,
 }
+
+-- return {
+--   'zbirenbaum/copilot.lua',
+--   event = 'VeryLazy',
+--   opts = {
+--     suggestion = { enabled = true, auto_trigger = true },
+--     panel = {
+--       enabled = false,
+--     },
+--     filetypes = {
+--       rust = false,
+--     },
+--   },
+--   config = function(_, opts)
+--     local copilot = require 'copilot'
+--     copilot.setup(opts)
+--
+--     local suggestion = require 'copilot.suggestion'
+--
+--     vim.keymap.set('i', '<C-j>', function()
+--       if suggestion.is_visible() then
+--         suggestion.accept()
+--       else
+--         suggestion.next()
+--       end
+--     end)
+--   end,
+-- }
