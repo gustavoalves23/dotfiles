2b649ace9b14aa4d65c0f86877ff5d28dfb18029
Gustavo Alves Miyazaki <gustavo.alves388@gmail.com>
2024-11-21 00:04:18 -0300

    feat: improve logic to getting true variables from tmux

--- nvim/lua/automation/scripts/tmux_get_true_alacritty_variables.sh
+++ nvim/lua/automation/scripts/tmux_get_true_alacritty_variables.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-alacritty_socket=$(tmux showenv | grep ALACRITTY_SOCKET | sed "s/ALACRITTY_SOCKET=//")
-alacritty_window_id=$(tmux showenv | grep ALACRITTY_WINDOW_ID | sed "s/ALACRITTY_WINDOW_ID=//")
+declare -A env_vars
 
-echo "{\"alacritty_socket\": \"$alacritty_socket\", \"alacritty_window_id\": \"$alacritty_window_id\"}"
+while IFS='=' read -r key value; do
+    if [[ $key == "ALACRITTY_SOCKET" || $key == "ALACRITTY_WINDOW_ID" ]]; then
+        env_vars[$key]=$value
+    fi
+done < <(tmux showenv)
+
+echo "{\"alacritty_socket\": \"${env_vars[ALACRITTY_SOCKET]}\", \"alacritty_window_id\": \"${env_vars[ALACRITTY_WINDOW_ID]}\"}"
