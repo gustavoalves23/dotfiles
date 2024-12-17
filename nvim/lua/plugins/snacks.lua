fd080d147e865f9689ba996f0a84fb5d7c9953cd
Gustavo Alves Miyazaki <gustavo.alves388@gmail.com>
2024-12-17 20:10:51 -0300

    feat: add nvim snacks

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,19 @@
+return {
+  'folke/snacks.nvim',
+  lazy = true,
+  cmd = { 'OpenOnBrowser', 'Lg', 'ZenMode' },
+  init = function()
+    local snacks = require 'snacks'
+    vim.api.nvim_create_user_command('OpenOnBrowser', function()
+      snacks.gitbrowse()
+    end, {})
+
+    vim.api.nvim_create_user_command('Lg', function()
+      snacks.lazygit()
+    end, {})
+
+    vim.api.nvim_create_user_command('ZenMode', function()
+      snacks.zen()
+    end, {})
+  end,
+}
