fd4aff3e7dc96af2b2034e36dabb0421bf835ee4
Gustavo Alves Miyazaki <gustavo.alves388@gmail.com>
2024-12-20 20:58:40 -0300

    feat: kitty automation when multiple nvim instances are running on the same terminal

--- nvim/lua/automation/kitty.lua
+++ nvim/lua/automation/kitty.lua
@@ -1,3 +1,7 @@
+local function get_config()
+  local str = vim.fn.system 'kitty @ ls'
+  return vim.json.decode(str)
+end
 local function add_padding()
   vim.fn.system 'kitten @ set-spacing padding=0'
   vim.fn.system 'kitten @ set-background-opacity 1'
@@ -5,7 +9,23 @@ end
 
 local function reset_config()
   vim.fn.system 'kitten @ set-spacing padding=14'
-  vim.fn.system 'kitten @ load-config'
+  local config = get_config()
+
+  local windows_running_vim = 0
+
+  for _, tab in pairs(config[1].tabs) do
+    for _, window in pairs(tab.windows) do
+      for _, process in pairs(window.foreground_processes) do
+        if process.cmdline[1] == 'nvim' then
+          windows_running_vim = windows_running_vim + 1
+        end
+      end
+    end
+  end
+
+  if windows_running_vim <= 1 then
+    vim.fn.system 'kitten @ load-config'
+  end
 end
 
 local _kitty_au = vim.api.nvim_create_augroup('kitty_padding_au', {
@@ -13,6 +33,7 @@ local _kitty_au = vim.api.nvim_create_augroup('kitty_padding_au', {
 })
 
 vim.api.nvim_create_user_command('Padding', add_padding, {})
+vim.api.nvim_create_user_command('Reset', reset_config, {})
 vim.api.nvim_create_autocmd('VimEnter', {
   group = _kitty_au,
   callback = add_padding,
