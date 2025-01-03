3d388814eb3a3a46eb0696722a2c087e6a3b30f3
Gustavo Alves Miyazaki <gmiyazaki@uolinc.com>
2025-01-03 08:51:19 -0300

    feat: optimize tmux automation with nvim

--- nvim/lua/automation/scripts/tmux_nvim_sessions.sh
+++ nvim/lua/automation/scripts/tmux_nvim_sessions.sh
@@ -9,8 +9,8 @@ vim_panes=0
 for window_id in $windows; do
     panes=$(tmux list-panes -t "$session_name:$window_id" -F "#P")
     for pane_id in $panes; do
-        command=$(tmux display-message -p -t "$session_name:$window_id.$pane_id" '#{pane_current_command}')
-        if [[ $command == "nvim" || $command == "sudoedit" ]]; then
+        pane_pid=$(tmux display-message -p -t "$session_name:$window_id.$pane_id" '#{pane_pid}')
+        if pstree -p "$pane_pid" | grep -qw "nvim"; then
             vim_panes=$((vim_panes + 1))
         fi
     done
